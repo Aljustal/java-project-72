d515d4bf5ba4585558c3480c337bebc532c3bf51
Aljustal <orbit.fsk@yandex.ru>
2022-12-04 13:32:19 +0300

    add directory config/checkstyle/checkstyle.xml

--- app/src/main/java/hexlet/code/controllers/RootController.java
+++ app/src/main/java/hexlet/code/controllers/RootController.java
@@ -3,5 +3,6 @@ package hexlet.code.controllers;
 import io.javalin.http.Handler;
 
 public class RootController {
-    public static final Handler welcome = ctx -> ctx.render("index.html");
+    public static final Handler Welcome = ctx -> ctx.render("index.html");
 }
+
